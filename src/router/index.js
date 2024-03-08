eef6ebcac490c2bd15158504cf50f148b4021bd5
Abdul Muqsit <abdulmuqsit987@gmail.com>
2024-03-08 15:39:07 +0500

    Second Layout

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
   {
     path: '/dashboard',
     name: 'dashboard',
-    component: () => import('../views/Home/HomePage.vue'), 
+    component: () => import('../views/Home/HomePage2.vue'), 
   },
   {
     path: '/login',
